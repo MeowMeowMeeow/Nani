f3293897e0c23afe91b13d97d26a02a646694cc2
MeowMeowMeeow <nathalie.davies3@gmail.com>
2025-03-03 15:04:23 +0800

    Updated DashboardScreen

--- app/src/main/java/com/example/nani/JairosoftApp.kt
+++ app/src/main/java/com/example/nani/JairosoftApp.kt
@@ -1,6 +1,12 @@
 package com.example.nani.screens.Dashboard
 
 
+import androidx.compose.foundation.layout.size
+
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 import android.content.res.Configuration
 import android.util.Log
 import androidx.annotation.StringRes
@@ -14,10 +20,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -25,12 +39,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -46,7 +61,6 @@ import com.example.nani.screens.Login.LoginViewModel
 import com.example.nani.screens.Profile.ProfileScreen
 import com.example.nani.screens.Projects.ProjectsScreen
 import com.example.nani.screens.Signup.SignUpScreen
-import com.example.nani.ui.theme.NaNiTheme
 import com.example.nani.ui.theme.components.bottomIconColor
 import com.example.nani.ui.theme.components.bottomIconImageColor
 import kotlinx.coroutines.delay
@@ -73,10 +87,16 @@ fun JairosoftApp() {
     )
 
     val loginResult by loginViewModel.loginResult.collectAsState()
+    var showBottomBarAndFab by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+
+        showBottomBarAndFab = true
+    }
 
     Scaffold(
         bottomBar = {
-            if (loginResult != null && currentScreen in listOf(
+            if (showBottomBarAndFab && loginResult != null && currentScreen in listOf(
                     JairosoftAppScreen.Dashboard,
                     JairosoftAppScreen.Analytics,
                     JairosoftAppScreen.Projects,
@@ -85,7 +105,29 @@ fun JairosoftApp() {
             ) {
                 JairosoftAppBar(navController)
             }
-        }
+        },
+        floatingActionButton = {
+            if (showBottomBarAndFab && loginResult != null && currentScreen in listOf(
+                    JairosoftAppScreen.Dashboard,
+                    JairosoftAppScreen.Analytics,
+                    JairosoftAppScreen.Projects,
+                    JairosoftAppScreen.Profile
+                )
+            ) {
+                FloatingActionButton(
+                    onClick = { /* FAB Action */ },
+                    shape = CircleShape,
+                    containerColor = Color.Green,
+                    elevation = FloatingActionButtonDefaults.elevation(12.dp),
+                    modifier = Modifier
+                        .size(80.dp)
+                        .offset(y = 50.dp)
+                ) {
+                    Icon(Icons.Default.Add, contentDescription = "Add", tint = Color.White)
+                }
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center
     ) { innerPadding ->
         NavHost(
             navController = navController,
@@ -96,7 +138,6 @@ fun JairosoftApp() {
                 val loginResult by loginViewModel.loginResult.collectAsState()
                 loginResult?.let {
                     LaunchedEffect(Unit) {
-                        delay(1000) // 1-second delay before navigation
                         navController.navigate(JairosoftAppScreen.Dashboard.name) {
                             popUpTo(JairosoftAppScreen.Login.name) { inclusive = true }
                         }
@@ -169,7 +210,9 @@ fun JairosoftAppBar(navController: NavController) {
                 }
             }
         }
+
     )
+
 }
 
 @Composable
